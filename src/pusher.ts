5396a21e480218fdf6566ca021d7acb34d128e12
andersea <andersa@atlab.dk>
2017-10-05 21:58:57 +0200

    Deleted a bunch of useless code.
    Classes suck.

--- src/pusher.ts
+++ src/pusher.ts
@@ -10,71 +10,6 @@ interface IPusherNodeProperties extends NodeRED.NodeProperties {
     event: string;
 }
 
-export = (RED: NodeRED.Red) => {
-    class PusherNode implements NodeRED.NodeOutputMixin, NodeRED.NodeInputMixin {
-        public send: (msg?: any) => void;
-        public on: (event: string | symbol, listener: (...args: any[]) => void) => this;
-
-        private connection: PusherConnection;
-        private subscriptions: ISubscription[] = [];
-
-        constructor(props: IPusherNodeProperties) {
-            RED.nodes.createNode(this, props);
-            this.connection = this.connect(props.appkey, props.cluster);
-            if (isSubscriptionSpec(props)) {
-                this.subscriptions.push(this.subscribe(props));
-            }
-            this.on('input', (msg: ISubscriptionMessage) => {
-                if (Array.isArray(msg.payload)) {
-                    msg.payload.filter(isSubscriptionSpec).forEach(val => {
-                        this.subscriptions.push(this.subscribe(val));
-                    });
-                } else {
-                    if (isSubscriptionSpec(msg.payload)) {
-                        this.subscriptions.push(this.subscribe(msg.payload));
-                    }
-                }
-            })
-            this.on('close', () => {
-                this.subscriptions.forEach(this.unsubscribe)
-                this.connection.disconnect();
-            })
-        }
-
-        private connect(appKey: string, cluster: string): PusherConnection {
-            return new Pusher(appKey, {
-                cluster,
-                encrypted: true
-            });
-        }
-
-        private subscribe(subscriptionSpec: ISubscriptionSpec): ISubscription {
-            const channel = this.connection.subscribe(subscriptionSpec.channel);
-            const eventHandler = (data: any) => {
-                const msg = {
-                    channel: subscriptionSpec.channel,
-                    payload: data.hasOwnProperty('payload') ? data.payload : data,
-                    topic: subscriptionSpec.event,
-                };
-                this.send(msg);
-            }
-            channel.bind(subscriptionSpec.event, eventHandler);
-            return {
-                channel,
-                event: subscriptionSpec.event,
-                eventHandler
-            };
-        }
-
-        private unsubscribe(subscription: ISubscription): void {
-            subscription.channel.unbind(subscription.event, subscription.eventHandler);
-            this.connection.unsubscribe(subscription.channel.name);
-        }
-
-    }
-    RED.nodes.registerType('pusher', PusherNode);
-}
-
 interface ISubscriptionMessage {
     payload: ISubscriptionSpec | ISubscriptionSpec[];
 }
@@ -88,9 +23,41 @@ function isSubscriptionSpec(msg: any): msg is ISubscriptionSpec {
     return msg.channel && msg.event;
 }
 
-interface ISubscription {
-    channel: Pusher.Channel;
-    event: string;
-    eventHandler: (data: any) => void;
-}
+export = (RED: NodeRED.Red) => {
+    function PusherNode(this: NodeRED.Node, props: IPusherNodeProperties) {
+        RED.nodes.createNode(this, props);
 
+        const connection = new Pusher(props.appkey, {
+            cluster: props.cluster,
+            encrypted: true
+        });
+        
+        const subscribe = (subscriptionSpec: ISubscriptionSpec): void => {
+            connection.subscribe(subscriptionSpec.channel).bind(subscriptionSpec.event, (data: any) => {
+                this.send({
+                    channel: subscriptionSpec.channel,
+                    payload: data.hasOwnProperty('payload') ? data.payload : data,
+                    topic: subscriptionSpec.event,
+                });
+            });
+        }
+        
+        if (isSubscriptionSpec(props)) {
+            subscribe(props);
+        }
+
+        this.on('input', (msg: ISubscriptionMessage) => {
+            if (Array.isArray(msg.payload)) {
+                msg.payload.filter(isSubscriptionSpec).forEach(val => {
+                    subscribe(val);
+                });
+            } else {
+                if (isSubscriptionSpec(msg.payload)) {
+                    subscribe(msg.payload);
+                }
+            }
+        });
+    }
+    
+    RED.nodes.registerType('pusher', PusherNode);
+}
